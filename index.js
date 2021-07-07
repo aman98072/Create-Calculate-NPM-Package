d5527eaa71d41295c72cdf39ef82af3693480100
Aman Gupta <aman98072@gmail.com>
2021-07-07 17:35:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+    add : (a, b) => {
+        return a + b;
+    },
+    substract : (a, b) => {
+        return a - b;
+    },
+    multiply : (a, b) => {
+        return a * b;
+    },
+    divide : (a, b) => {
+        return a / b;
+    }
+}
